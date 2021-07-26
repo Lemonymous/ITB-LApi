a493b9c6c0faf1f44b90f7adc42cd37a8cb8ddd3
Lemonymous <10490534+Lemonymous@users.noreply.github.com>
2021-07-26 17:56:18 +0200

    Fixed bug where all options were treated as enabled

--- modules/cutils.lua
+++ modules/cutils.lua
@@ -13,10 +13,15 @@ local cutils = {}
 function cutils:init()
 	local currentModContent = mod_loader.currentModContent
 	local options = currentModContent and currentModContent[mod_id].options or {}
+
+	local cutils_debug = options.cutils_debug or {}
+	local cutils_verbose_init = options.cutils_verbose_init or {}
+	local cutils_verbose_calls = options.cutils_verbose_calls or {}
+
 	local ok, err = pcall(package.loadlib(cutilsPath, "luaopen_inspect"), {
 		name = NAME,
-		debug = options.cutils_debug or false,
-		verbose = options.cutils_verbose_init or false,
+		debug = cutils_debug.enabled or false,
+		verbose = cutils_verbose_init.enabled or false,
 		get = true,
 		set = true,
 	})
@@ -27,7 +32,7 @@ function cutils:init()
 		LOGF("%s into global table _G[\"%s\"]", successMsg, NAME)
 	end
 
-	if options.cutils_verbose_calls then
+	if cutils_verbose_calls.enabled then
 		for name, fn in pairs(_G[NAME].Pawn) do
 			if type(fn) == 'function' then
 				local oldFn = fn
