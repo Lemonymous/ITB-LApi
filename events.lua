6430bbb21d3f320d11a6fdcc746ae09dbf2cd27d
Lemonymous <10490534+Lemonymous@users.noreply.github.com>
2022-03-13 23:40:29 +0100

    Add board tile events

---
+++ events.lua
@@ -0,0 +1,34 @@
+
+local events = {
+	"onTileTerrainChanged",
+	"onTileHealthChanged",
+	"onTileMaxHealthChanged",
+	"onTileDamaged",
+	"onTileBuildingDamaged",
+	"onTileBuildingDestroyed",
+	"onTileUniqueBuildingDestroyed",
+	"onTileUniqueBuildingNameChanged",
+	"onTileItemNameChanged",
+	"onTileIsBuilding",
+	"onTileIsUniqueBuilding",
+	"onTileIsItem",
+	"onTileIsShield",
+	"onTileIsFrozen",
+	"onTileIsSmoke",
+	"onTileIsFire",
+	"onTileIsAcid",
+	"onTileIsNotBuilding",
+	"onTileIsNotUniqueBuilding",
+	"onTileIsNotItem",
+	"onTileIsNotShield",
+	"onTileIsNotFrozen",
+	"onTileIsNotSmoke",
+	"onTileIsNotFire",
+	"onTileIsNotAcid",
+}
+
+for _, eventId in ipairs(events) do
+	if modApi.events[eventId] == nil then
+		modApi.events[eventId] = Event()
+	end
+end
